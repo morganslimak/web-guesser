dfd2add1bc7e5fbdd2931b417098f85a93a85e50
Morgan Slimak <morganslimak@gmail.com>
2016-09-27 17:31:49 -0600

    changes background color depending on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,8 +5,8 @@ require 'pry'
 NUMBER = rand(101)
 
 def check_guess(guess)
-  return if guess.nil?
   message = case
+    when guess.nil? then "Guess a number!"
     when guess.to_i - 5 > NUMBER then "Way too high!"
     when guess.to_i + 5 < NUMBER then "Way too low!"
     when guess.to_i > NUMBER then "Too high!"
@@ -15,8 +15,20 @@ def check_guess(guess)
   end
 end
 
+def set_color(guess)
+  color = case
+    when guess.nil? then "#ffffff"
+    when guess.to_i - 5 > NUMBER || guess.to_i + 5 < NUMBER then "#FF170D"
+    when guess.to_i > NUMBER || guess.to_i < NUMBER then "#780B06"
+    when guess.to_i == NUMBER then "#11FF0D"
+  end
+end
+
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:number => NUMBER, :message => message}
+  color = set_color(guess)
+  erb :index, :locals => {:number => NUMBER,
+                          :message => message,
+                          :color => color}
 end
